6a340a133017de07044913cbe4bf3370c239c96c
1069683910@qq.com <mmmolivia@mmmoliviadeMacBook-Air.local>
2024-03-31 15:13:12 +0800

    个人主页搭建

---
+++ pages/AddPost.tsx
@@ -0,0 +1,11 @@
+import AddPost from "../components/AddPostComponent";
+
+export default function Person() {
+    return (
+        <>
+        <div style={{ width: "100%", height: "1040px"}}>
+            <AddPost />
+        </div>
+    </>
+    )
+}
