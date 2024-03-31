4e50c19b4e0a3f99d5620137ea9ff979611dd4c3
1069683910@qq.com <mmmolivia@mmmoliviadeMacBook-Air.local>
2024-03-31 14:44:18 +0800

    个人主页创建

---
+++ pages/person.tsx
@@ -0,0 +1,13 @@
+
+import PersonLogin from "../components/PersonLogin";
+
+export default function Person() {
+    return (
+        <>
+        <div style={{ width: "100%", height: "1040px"}}>
+            <PersonLogin />
+        </div>
+
+    </>
+    )
+}
