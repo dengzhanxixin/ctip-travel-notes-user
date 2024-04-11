f310c3c66d304ac0e0974212800aba4330e5ed0e
ganshengxiafan <1726446284@qq.com>
2024-04-11 10:42:44 +0800

    微信分享功能

--- pages/api/serve.js
+++ pages/api/serve.js
@@ -4,14 +4,17 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const bcrypt = require("bcrypt");
-const saltRounds = 10; // 定义salt的回合数，推荐值
+// sha1加密
+const sha1 = require("sha1");
 
+const saltRounds = 10; // 定义salt的回合数，推荐值
 
 const path = require("path");
 // 从 page/api/ 目录向上两级，然后进入 data 目录
 const dataPath = path.join(__dirname, "..", "..", "data", "user.json");
-const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'ctrip_yhr_secret_key'; // 用于JWT加密的密钥，应保持安全
+const jwt = require("jsonwebtoken");
+const { createInputFiles } = require("typescript");
+const SECRET_KEY = "ctrip_yhr_secret_key"; // 用于JWT加密的密钥，应保持安全
 const port = 3001;
 
 app.use(express.json()); // 用于解析JSON的中间件
@@ -102,6 +105,71 @@ app.get("/api/getpost", (req, res) => {
   res.json(postDataArray);
   console.log("One post data:", postDataArray[0]);
 });
+
+// 微信分享后端接口实现
+
+// 接口配置信息测试
+app.get("/api/wxJssdk", (req, res) => {
+  let wx = req.query;
+
+  let token = "ctrip_wexinShare";
+  let timestamp = wx.timestamp;
+  let nonce = wx.nonce;
+
+  // 将token、timestamp、nonce三个参数进行字典排序
+  let list = [token, timestamp, nonce].sort();
+
+  // 将三个参数字符串拼接成一个字符串进行sha1加密
+  let str = list.join("");
+  let result = sha1(str);
+
+  // 3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+  if (result === wx.signature) {
+    res.send(wx.echostr); // 返回微信传来的echostr，表示校验成功，此处不能返回其它
+  } else {
+    res.send(false);
+  }
+});
+
+// 微信分享后端接口实现
+app.post("/api/wxJssdk/getJssdk", (req, res) => {
+  const grant_type = "client_credential";
+  // 测试号
+  const appid = "wx7038d3636b5797ed";
+  const secret = "bd3dbda5b59c8f6c1057fb9edd163acd";
+
+  request(
+    "https://api.weixin.qq.com/cgi-bin/token?grant_type=" + grant_type + "&appid=" + appid + "&secret=" + secret,
+    (err, response, body) => {
+      let access_token = JSON.parse(body).access_token;
+
+      request(
+        "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + access_token + "&type=jsapi",
+        (err, response, body) => {
+          let jsapi_ticket = JSON.parse(body).ticket;
+          let nonce_str = "123456"; // 密钥，字符串任意，可以随机生成
+          let timestamp = new Date().getTime(); // 时间戳
+          let url = req.query.url; // 使用接口的url链接，不包含#后的内容
+
+          // 将请求以上字符串，先按字典排序，再以'&'拼接，如下：其中j > n > t > u，此处直接手动排序
+          let str =
+            "jsapi_ticket=" + jsapi_ticket + "&noncestr=" + nonce_str + "&timestamp=" + timestamp + "&url=" + url;
+
+          // 用sha1加密
+          let signature = sha1(str);
+
+          res.send({
+            appId: appid,
+            timestamp: timpstamp,
+            nonceStr: nonce_str,
+            signature: signature,
+          });
+        }
+      );
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
